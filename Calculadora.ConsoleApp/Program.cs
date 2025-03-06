aef2513f0de6db4a277d6db91e4d88c0403d7b91
Victor R. Silva <victorr.silva005@gmail.com>
2025-03-06 17:05:49 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Tabajara 2025");
+
+            Console.Write("Digite o primeiro número: ");
+            string primerioNumeroString = Console.ReadLine();
+            int primeiroNumero = Convert.ToInt32(primerioNumeroString);
+
+            Console.Write("Digite o segundo número: ");
+            string segundoNumeroString = Console.ReadLine();
+            int segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+            int resultado = primeiroNumero + segundoNumero;
+
+            Console.WriteLine(resultado);
+
+            Console.ReadKey();
+        }
+    }
+}
